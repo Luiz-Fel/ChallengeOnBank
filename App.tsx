52f19cefbf8da87c4a9fac46ce04e989b93fe713
Luiz Felipe <lfsprj@gmail.com>
2022-03-16 17:56:09 -0300

    Frist commit.

--- App.tsx
+++ App.tsx
@@ -1,12 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { Header } from './components/header';
 
 export default function App() {
   return (
+    <>
+    <Header />
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text>Hello world!</Text>
     </View>
+    </>
   );
 }
 
---
+++ components/header.tsx
@@ -0,0 +1,17 @@
+import { StyleSheet, Text, View } from 'react-native';
+
+
+export function Header() {
+
+    return(
+        <View style={styles.header}>    
+            <Text>Hello</Text>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    header: {
+        
+    }
+})
